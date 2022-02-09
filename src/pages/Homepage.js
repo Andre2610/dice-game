522a29e761ce01505fb607c245e8c063b820ec9e
andre2610 <dre2610@gmail.com>
2022-02-09 16:09:39 +0100

    added target score, and userfeedback for the winner

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -15,9 +15,10 @@ const initialScore = [
 export default function Homepage() {
   const [playerOneTurn, setPlayerOneTurn] = useState(true);
   const [score, setScore] = useState(initialScore);
-  const [p1Roll, setP1Roll] = useState(null);
-  const [p2Roll, setP2Roll] = useState(null);
-  const [userFeedback, setUserFeedback] = useState('');
+  const [targetSCore, setTargetSCore] = useState(5);
+  const [p1Roll, setP1Roll] = useState(0);
+  const [p2Roll, setP2Roll] = useState(0);
+  const [userFeedback, setUserFeedback] = useState(null);
 
   const diceRoll = () => {
     const roll = Math.ceil(Math.random() * 6);
@@ -41,12 +42,21 @@ export default function Homepage() {
     );
     setScore(newScore);
 
-    endTurn(`Player ${turnWinnerId} won the turn!`);
+    endTurn(`Player ${turnWinnerId} won the turn!`, newScore);
   };
-  const endTurn = (message) => {
-    setUserFeedback(message);
-    setP1Roll(null);
-    setP2Roll(null);
+  const endTurn = (message, newScore) => {
+    const winner = newScore.find((player) => player.score === targetSCore);
+    const winnerMessage =
+      winner &&
+      `We have a winner! Player ${winner.id} reached ${targetSCore} points!`;
+
+    setUserFeedback({
+      ...userFeedback,
+      winnerMessage,
+      turnMessage: message,
+    });
+    setP1Roll(0);
+    setP2Roll(0);
   };
 
   console.log(`Player one roll: ${p1Roll} | Player two roll: ${p2Roll}`);
@@ -54,10 +64,16 @@ export default function Homepage() {
   return (
     <div style={{ margin: '0 auto', textAlign: 'center' }}>
       <ScoreTable score={score} />
-      {userFeedback && <p>{userFeedback}</p>}
+
       <button onClick={() => (p1Roll && p2Roll ? compareScore() : diceRoll())}>
-        Roll
+        {p1Roll && p2Roll ? 'Compare scores' : 'Roll'}
       </button>
+      <p>
+        <span>{p1Roll}</span> -{' '}
+        {userFeedback && userFeedback.turnMessage + ' - '}
+        <span>{p2Roll}</span>
+      </p>
+      {userFeedback && <p>{userFeedback.winnerMessage}</p>}
     </div>
   );
 }
